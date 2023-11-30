1b2a65c07c7fbaa701e1f040235935280bde3842
Nolram567 <lucif3r@posteo.de>
2023-11-30 13:04:08 +0100

    - Draw different graphs

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
-import scipy as sp
+from networkx.algorithms import bipartite
+from networkx.algorithms import centrality
 
 if __name__ == '__main__':
 
     df = pd.read_csv("Soziomatrix - Tabellenblatt1.csv")
 
-    G = nx.DiGraph()
+    G = nx.Graph()
 
     individuals = {}
     opportunities = []
@@ -24,12 +25,12 @@ if __name__ == '__main__':
         individuals[df.iloc[row].iloc[0]] = df.iloc[row].iloc[1:]
 
 
-    print(list(individuals.keys()))
-    print(opportunities)
+    '''print(list(individuals.keys()))
+    print(opportunities)'''
 
     G.add_nodes_from(list(individuals.keys()), bipartite=0)
     G.add_nodes_from(opportunities, bipartite=1)
-    print(individuals["Name IX"].iloc[0])
+
 
     for i in individuals.keys():
         for o, e in list(individuals[i].items()):
@@ -39,12 +40,29 @@ if __name__ == '__main__':
 
     G.add_edges_from(edgelist)
 
-    pos = nx.bipartite_layout(G, nodes=list(individuals.keys()))  # bipartite_layout zur Positionierung der Knoten
-    nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, cmap=plt.cm.Paired)
+    pos1 = nx.bipartite_layout(G, nodes=list(individuals.keys()))
+    pos2 = nx.spring_layout(G)
+    nx.draw(G, pos1, with_labels=True, font_weight='bold', node_size=700,
+            node_color=['skyblue' if n in ['Cafè Wertvoll', 'VB Kletterhalle', 'Resteraunt Dromedar', 'Cavete', 'Cineplex',
+                                           'Basic Coffee', 'Restaurant Mon Ami', 'Waggonhalle']
+                        else 'salmon' for n in G.nodes()])
+
     plt.title('Bipartiter Graph')
     plt.show()
 
-    min_shared_opps = 2
+
+    #print(bipartite.clustering(G, list(G.nodes)[0:10:None]))
+    G = nx.bipartite.projected_graph(G, list(G.nodes)[0:10:None])
+
+    print(f"degree centrality : {centrality.degree_centrality(G)}\n")
+    print(f"betweenness centraility : {centrality.betweenness_centrality(G)}\n")
+    print(f"betweenness centraility : {centrality.closeness_centrality(G)}\n")
+
+    # Zeige den unipartiten Graphen
+    nx.draw(G, with_labels=True, font_weight='bold', node_color='skyblue')
+    plt.show()
+
+    min_shared_opps = 3
 
     G_unimodal = nx.Graph()
     edgelist_unimodal = []
@@ -63,7 +81,11 @@ if __name__ == '__main__':
 
     pos = nx.kamada_kawai_layout(G_unimodal)
 
-    nx.draw(G_unimodal, pos, with_labels=True, font_weight='bold', node_size=700, cmap=plt.cm.Paired)
+    nx.draw(G_unimodal, pos, with_labels=True, font_weight='bold', node_size=700)
+
+    print(f"degree centrality : {centrality.degree_centrality(G_unimodal)}\n")
+    print(f"betweenness centraility : {centrality.betweenness_centrality(G_unimodal)}\n")
+    print(f"betweenness centraility : {centrality.closeness_centrality(G_unimodal)}\n")
 
     plt.show()
 
